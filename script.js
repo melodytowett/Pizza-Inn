9899c3ee6b21bc9bfe136038901f4dd0f29ff0dd
melodytowett <melodytowett99@gmail.com>
2022-01-15 11:25:28 +0300

    assign size price

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ $(document).ready(function () {
         $('#hidden').show();
     });
 })
-class Pizza{
-    constructor(size, crust, toppings,number){
+// let mySize, myCrust,myToppings,myNumber;
+    function pizza (size, crust, toppings,number){
         this.size = size;
         this.crust = crust;
         this.toppings = toppings;
         this.number = number;
     }
-    $
-}
+    var sizecost,crustCost,toppingsCost,numberPizza;
+    $(document).ready(function () {
+        $('#myForm').submit(function (event) {
+            event.preventDefault();
+            var pizzaSize = $('#size').val();
+            var pizzaCrust = $('#crust').val();
+            var pizzaToppings = $('#toppings').val();
+            var pizzaNumber = $('#number').val();
+            var myPizza = new pizza(pizzaSize, pizzaCrust,pizzaToppings,pizzaNumber);
+            if (myPizza.size == "Large") {
+                sizecost = 800;
+            }
+            else if (myPizza.size == "Medium") {
+                sizecost == 400;
+            }
+            else{
+                sizecost == 200
+            }
+        })
+    })
