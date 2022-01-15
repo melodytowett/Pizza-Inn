08debb6f8ad132353eb871c1b9f9d48c77e1d0b6
melodytowett <melodytowett99@gmail.com>
2022-01-15 10:24:17 +0300

    start the business logic

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.btn').click(function (event) {
+        event.preventDefault();
+        $('#hidden').show();
+    });
+})
+class Pizza{
+    constructor(size, crust, toppings,number){
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.number = number;
+    }
+    $
+}
