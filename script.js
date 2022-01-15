cf932e93c6e95b4db469f1645230fe6e0e2515c4
melodytowett <melodytowett99@gmail.com>
2022-01-16 01:16:44 +0300

    add the delivery funtion  and prices

--- script.js
+++ script.js
@@ -6,13 +6,14 @@
 //     });
 // })
 // let mySize, myCrust,myToppings,myNumber;
-    function pizza (size, crust, toppings,number){
+    function pizza (size, crust, toppings,number,deliver){
         this.size = size;
         this.crust = crust;
         this.toppings = toppings;
         this.number = number;
+        this.deliver = deliver;
     }
-    var sizeCost,crustCost,toppingsCost,totalCost,pizzaNumber;
+    var sizeCost,crustCost,toppingsCost,totalCost,pizzaNumber,deliveryCost;
     $(document).ready(function () {
         $('#myForm').submit(function (event) {
             event.preventDefault();
@@ -20,7 +21,8 @@
             var pizzaCrust = $('#crust').val();
             var pizzaToppings = $('#toppings').val();
             var pizzaNumber = parseInt($('#number').val());
-            var myPizza = new pizza(pizzaSize, pizzaCrust,pizzaToppings,pizzaNumber);
+            var pizzaDelivery = $('#deliver').val();
+            var myPizza = new pizza(pizzaSize, pizzaCrust,pizzaToppings,pizzaNumber,pizzaDelivery);
             // pizza cost against size
             if (myPizza.size == "Large") {
                 sizeCost = 800;
@@ -41,32 +43,41 @@
             }
             //pizza price with toppings
             if (myPizza.toppings == 'pinneaple') {
-                toppingsCost = 30;
+                toppingsCost = 130;
             }else if (myPizza.toppings == 'beef') {
-                toppingsCost = 40
+                toppingsCost = 240
             }else{
-                toppingsCost = 60;
+                toppingsCost = 360;
             }
            totalCost = (sizeCost + crustCost + toppingsCost)*pizzaNumber;
+           if (myPizza.deliver == "yes") {
+               deliveryCost = 200;
+                var loc = prompt("enter location")
+                alert( 'delivery cost is 200')
+                alert('Your order will be delivered to ' +loc)
+           }else{
+               deliveryCost = 0;
+               $('#out').hide();
+           }
+           var newPrice = (totalCost + deliveryCost)
+          // alert (newPrice);
+
+           //alert(toppingsCost)
            $('#hidden').show();
            $("#quantity").html(pizzaNumber);
            $("#Total").html(totalCost);
            $("#type").html(pizzaCrust);
            $("#top").html(pizzaToppings);
+           $('#Location').html(loc);
+           $('#amount').html(newPrice);
            //alert(totalCost);
-           $(".Clickable").click(function () {
-               $("#new-contact").toggle();
+        //    $(".Clickable").click(function () {
+        //        $("#new-contact").toggle();
+        //    })
+        //    $("#add-address").click(function () {
+        //        $("#out").toggle();
+        //    })
            })
-           $("#add-address").click(function () {
-               $("#out").toggle();
-           })
-           })
-           $('#new-contact').submit(function (event) {
-               event.preventDefault();
-               var phoneNo = $("#phne").val();
-               var street = $("#street").val();
-               var county = $("#county").val();
 
-           })
         })
     
